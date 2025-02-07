42819b8ab7d854c033f5b7fdf5c185886a1b0d19
Suryansu sanjeebani Mohanty <ssanjeebanimohanty@gmail.com>
2025-02-07 07:11:22 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // Correct import
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter , Routes, Route} from "react"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename = "/TO-DO-app">
-    <Routes>
-      <Route path="*" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/TO-DO-app">
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
